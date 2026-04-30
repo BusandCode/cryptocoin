a83220d51d3e047c47fe0ab7e22a6b59aa6da07a
Adetokunbo Andrew <adetokunboandrew2@gmail.com>
2026-04-30 13:13:17 +0100

    cryptocoin update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
-import HomePage from "./HomePage";
+import HomePage from "./";
 const App = () => {
   return (
     <BrowserRouter>
